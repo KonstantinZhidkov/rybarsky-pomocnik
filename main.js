302400f8eba361ab30c36a7278921d310ef1c5ab
KonstantinZhidkov <konstantin.zhidkov1997@gmail.com>
2020-10-26 02:30:03 +0100

    Menu hover feature is finished.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
-    // Navigace
+    // Navigace burger
     $('.header__toggle').on('click', () => {
         $('.header__toggle, .header__burger, .header__navigation').toggleClass('active');
         $('body').toggleClass('block');
@@ -16,5 +16,26 @@ $(document).ready(() => {
         } else {
             $('.header__body').removeClass('nonstick');
         }
+    });
+
+    // Navigace hover efekt (testuji čistý JavaScript + jQuery)
+    let headerUnderline = document.querySelector('#header__underline');
+    let headerItem = document.querySelectorAll('.header__item');
+
+    function setUnderline(navElem) {
+        headerUnderline.style.left = navElem.offsetLeft + "px";
+        headerUnderline.style.width = navElem.offsetWidth + "px";
+    }
+
+    headerItem.forEach(item => {
+        item.addEventListener('mouseenter', event => {
+            setUnderline(event.target);
+        });
+    });
+
+    $('.header__list').on('mouseleave', () => {
+        $('#header__underline').css('visibility', 'hidden');
+    }).on('mouseenter', () => {
+        $('#header__underline').css('visibility', 'visible');
     });
 });
