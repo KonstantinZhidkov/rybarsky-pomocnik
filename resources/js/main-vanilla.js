db1d7cab5910a3a7c4cd2810376a7420855629d8
KonstantinZhidkov <konstantin.zhidkov1997@gmail.com>
2021-03-02 23:12:37 +0100

    Input and button interactivity in weather block added. JavaScript logic in weather block was partly added.

--- resources/js/main-vanilla.js
+++ resources/js/main-vanilla.js
@@ -21,3 +21,47 @@ headerList.onmouseleave = () => {
 headerList.onmouseenter = () => {
     headerUnderline.style.visibility = 'visible';
 }
+
+// Počasí block
+
+const enterAreaInput = document.querySelector('.enterArea__input');
+const enterAreaSubmitButton = document.querySelector('.enterArea__submitButton');
+const api = {
+    key: "b42ee8f264297822fbae61139212bbd8",    // Můj unikátní klíč
+    url: "https://api.openweathermap.org/data/2.5/"
+}
+
+enterAreaSubmitButton.addEventListener('click', () => {
+    getResults(enterAreaInput.value);
+});
+
+function getResults(param) {
+    fetch(`${api.url}weather?q=${param}&units=metric&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    let mesto = document.querySelector('.pocasi__mesto');
+    let datum = document.querySelector('.pocasi__datum');
+    let teplota = document.querySelector('.pocasi__teplota');
+    let now = new Date();
+
+    mesto.innerText = `${weather.name}, ${weather.sys.country}`;
+    datum.innerText = datumGenerator(now);
+    teplota.innerHTML = `${Math.round(weather.main.temp)}&deg;`;
+}
+
+function datumGenerator(param) {
+    const mesice = ["Ledena", "Února", "Březena", "Dubna", "Května", "Června", "Července", "Srpena", "Září", "Října", "Listopadu", "Prosince"];
+    const dny = ["Pondělí", "Úterý", "Středa", "Čtvrtek", "Pátek", "Sobota", "Neděle"];
+
+    let den = dny[param.getDay()];
+    let datum = param.getDate();
+    let mesic = mesice[param.getMonth()];
+    let rok = param.getFullYear();
+
+    return `${den} ${datum} ${mesic} ${rok}`;
+}
