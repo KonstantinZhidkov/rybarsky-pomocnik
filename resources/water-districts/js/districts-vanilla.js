7fb33a220a92614c8969ec834b000f5427511426
KonstantinZhidkov <konstantin.zhidkov1997@gmail.com>
2021-07-02 23:36:03 +0200

    The javascript logic for popup window of water districts gallery coded.

--- resources/water-districts/js/districts-vanilla.js
+++ resources/water-districts/js/districts-vanilla.js
@@ -54,6 +54,85 @@ document.querySelectorAll('.revirGallery__navigationItem').forEach(item => {
 document.querySelector('.revirGallery__navigationItem').click();
 
 // Galerie
+
+const popup = document.querySelector('.popup');
+const revirGalleryImages = document.querySelectorAll('.revirGallery__image');
+const body = document.querySelector('body');
+const lockPadding = document.querySelector('.lock-padding');
+const popupCloseIcon = document.querySelector('.popup__close');
+const timeout = 800;
+let unlock = true;
+
+revirGalleryImages.forEach(image => {
+    image.addEventListener('click', function (event) {
+        popupOpen(popup);
+        popup.classList.add('open');
+    });
+});
+
+popupCloseIcon.addEventListener('click', function(event) {
+    event.preventDefault();
+    popupClose(event.target.closest('.popup'));
+});
+
+function popupOpen(currentPopup) {
+    if(currentPopup && unlock) {
+        bodyLock();
+    }
+    currentPopup.addEventListener('click', function (event) {
+       if(!event.target.closest('.popup__content')) {
+           popupClose(event.target.closest('.popup'));
+       }
+    });
+}
+
+function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector('.revirGallery').offsetWidth + "px";
+
+    lockPadding.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
+    unlock = false;
+    setTimeout(function() {
+        unlock = true;
+    }, timeout);
+}
+
+function popupClose(popupActive) {
+    popupActive.classList.remove('open');
+    bodyUnlock();
+}
+
+function bodyUnlock() {
+    setTimeout(function() {
+        lockPadding.style.paddingRight = '0px';
+        body.style.paddingRight = '0px';
+        body.classList.remove('lock');
+    }, timeout);
+
+    unlock = false;
+    setTimeout(function() {
+        unlock = true;
+    }, timeout);
+}
+
+document.addEventListener('keydown', function(event) {
+    if(event.which == 27) {
+        popupClose(document.querySelector('.popup.open'));
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 const revirGalleryImages = document.querySelectorAll('.revirGallery__image');
 
